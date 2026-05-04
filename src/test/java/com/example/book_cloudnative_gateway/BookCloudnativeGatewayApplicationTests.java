7a0bb36aab1d4e543203b16c19ac461a77bca121
alerodrigues <ale.rodrigues@live.com>
2026-05-04 18:06:34 +0000

    first commit

---
+++ src/test/java/com/example/book_cloudnative_gateway/BookCloudnativeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_cloudnative_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCloudnativeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
